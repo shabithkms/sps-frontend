31ae5d56156da6b93b14086ab16243a792e8d97b
shabithkms <shabithkms2035@gmail.com>
2022-02-15 12:45:35 +0530

    admin side session handling

--- src/Pages/Admin/AdminHome.jsx
+++ src/Pages/Admin/AdminHome.jsx
@@ -1,22 +1,8 @@
-import React, { useEffect } from "react";
-import Sidebar from "../../Components/Admin/AdminSidebar/Sidebar";
+import React from "react";
 import Dashboard from "../../Components/Admin/AdminDashboard/Dashboard";
-import { useNavigate } from "react-router";
+import Sidebar from "../../Components/Admin/AdminSidebar/Sidebar";
 
 function AdminHome(props) {
-  console.log(props);
-  const navigate = useNavigate();
-  useEffect(() => {
-    let ADMIN = process.env.REACT_APP_ADMIN;
-    let Admin = localStorage.getItem("Admin");
-    console.log(Admin);
-    if (Admin) {
-      navigate("/admin");
-    } else {
-      navigate("/admin/login");
-    }
-    return () => {};
-  }, []);
 
   return (
     <div className="d-flex">
--- src/Pages/Admin/AdminLogin.jsx
+++ src/Pages/Admin/AdminLogin.jsx
@@ -1,19 +1,6 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router";
 import Login from "../../Components/Auth/AdminLogin";
 
 function AdminLogin() {
-  const navigate = useNavigate();
-  useEffect(() => {
-    let ADMIN = process.env.REACT_APP_ADMIN;
-    let Admin = localStorage.getItem("Admin");
-    if (Admin) {
-      navigate("/admin");
-    } else {
-      navigate("/admin/login");
-    }
-    return () => {};
-  }, []);
 
   return (
     <div>
