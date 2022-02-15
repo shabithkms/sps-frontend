ec0dd421610815b94574a870f12ce53ca0725120
shabithkms <shabithkms2035@gmail.com>
2022-02-15 12:44:20 +0530

    folder structure changed , routes are added to seperate file

--- src/App.js
+++ src/App.js
@@ -1,82 +1,38 @@
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
-import { Routes, Route } from 'react-router';
-// Impoorting Admin Pages
-import AdminLogin from './Pages/Admin/AdminLogin';
-import AdminHome from './Pages/Admin/AdminHome';
-import Batches from './Pages/Admin/BatchManagement';
-import TeacherList from './Pages/Admin/TeacherList';
-
-// Importing Teacher Pages
-import TeacherLogin from './Pages/Teacher/TeacherLogin';
-import TeacherSignup from './Pages/Teacher/TeacherSignup';
-import TeacherHome from './Pages/Teacher/TeacherHome';
-import TeacherProfile from './Pages/Teacher/TeacherProfile';
-import EditTeacherProfile from './Pages/Teacher/EditTeacherProfile';
-import Students from './Pages/Teacher/StudentManagement';
-import Domains from './Pages/Teacher/DomainManagement';
-// Importing Reviewer pages
-import Reviewer from './Pages/Teacher/ReviewerManagement';
-
-// Importing Student pages
-import StudentHome from './Pages/Student/Home';
-import StudentSignup from './Pages/Student/Signup';
-import StudentsLogin from './Pages/Student/Login';
-import StudentProfile from './Pages/Student/Profile';
-import StudentEditProfile from './Pages/Student/EditTeacherProfile';
-
-// Importing Reviewer Pages
-import ReviewerHome from '../src/Pages/Reviewer/Home';
-import ReviewerLogin from '../src/Pages/Reviewer/Login';
-import ReviewerSignup from '../src/Pages/Reviewer/Signup';
+import Routed from './Routes';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <div className='App'>
-      <Routes>
-        {/* Admin Routes */}
-        <Route path='/admin' element={<AdminHome />}></Route>
-        <Route path='/admin/teachers' element={<TeacherList />}></Route>
-        <Route path='/admin/batches' element={<Batches />}></Route>
-        <Route path='/admin/login' element={<AdminLogin />}></Route>
-
-        {/* Teacher routes */}
-        {/* Teacher Auth */}
-        <Route path='/teacher/login' element={<TeacherLogin />}></Route>
-        <Route path='/teacher/:token' element={<TeacherSignup />} />
-        {/* Dashboard */}
-        <Route path='/teacher' element={<TeacherHome />} />
-        {/* Profile Management */}
-        <Route path='/teacher/profile/:id' element={<TeacherProfile />} />
-        <Route path='/teacher/profile/' element={<TeacherLogin />} />
-        <Route
-          path='/teacher/editProfile/:id'
-          element={<EditTeacherProfile />}
-        ></Route>
-        <Route
-          path='/teacher/editProfile/:id'
-          element={<EditTeacherProfile />}
-        />
-        {/* Reviewer Management */}
-        <Route path='/teacher/reviewer' element={<Reviewer />} />
-
-        {/* Student management */}
-        <Route path='/teacher/students' element={<Students />}></Route>
-
-        {/* Domain management */}
-        <Route path='/teacher/domains' element={<Domains />}></Route>
-
-        {/* Student routes */}
-        <Route path='/' element={<StudentHome />}></Route>
-        <Route path='/signup' element={<StudentSignup />}></Route>
-        <Route path='/login' element={<StudentsLogin />}></Route>
-        <Route path='/profile' element={<StudentProfile />}></Route>
-        <Route path='/editProfile' element={<StudentEditProfile />}></Route>
-
-        {/* Reviewer Routes */}
-        <Route path='/reviewer' element={<ReviewerHome />} />
-        <Route path='/reviewer/login' element={<ReviewerLogin />} />
-        <Route path='/reviewer/register/:token' element={<ReviewerSignup />} />
-      </Routes>
+      <Toaster
+        position='top-center'
+        reverseOrder={false}
+        gutter={8}
+        containerClassName=''
+        containerStyle={{}}
+        toastOptions={{
+          // Define default options
+          className: '',
+          duration: 4000,
+          style: {
+            background: 'black',
+            color: '#fff',
+          },
+          // Default options for specific types
+          success: {
+            duration: 3000,
+            theme: {
+              primary: 'green',
+              secondary: 'black',
+            },
+          },
+        }}
+      />
+      <Router>
+        <Routed />
+      </Router>
     </div>
   );
 }
---
+++ src/Routes.js
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from 'react';
+
+import { Route, Routes, Navigate, useLocation } from 'react-router-dom';
+
+// Importing Reviewer Pages
+import ReviewerHome from '../src/Pages/Reviewer/Home';
+import ReviewerLogin from '../src/Pages/Reviewer/Login';
+import ReviewerSignup from '../src/Pages/Reviewer/Signup';
+import AdminHome from './Pages/Admin/AdminHome';
+// Impoorting Admin Pages
+import AdminLogin from './Pages/Admin/AdminLogin';
+import Batches from './Pages/Admin/BatchManagement';
+import TeacherList from './Pages/Admin/TeacherList';
+import StudentEditProfile from './Pages/Student/EditTeacherProfile';
+// Importing Student pages
+import StudentHome from './Pages/Student/Home';
+import StudentsLogin from './Pages/Student/Login';
+import StudentProfile from './Pages/Student/Profile';
+import StudentSignup from './Pages/Student/Signup';
+import Domains from './Pages/Teacher/DomainManagement';
+import EditTeacherProfile from './Pages/Teacher/EditTeacherProfile';
+// Importing Reviewer pages
+import Reviewer from './Pages/Teacher/ReviewerManagement';
+import Students from './Pages/Teacher/StudentManagement';
+import TeacherHome from './Pages/Teacher/TeacherHome';
+// Importing Teacher Pages
+import TeacherLogin from './Pages/Teacher/TeacherLogin';
+import TeacherProfile from './Pages/Teacher/TeacherProfile';
+import TeacherSignup from './Pages/Teacher/TeacherSignup';
+
+function Routed() {
+  const [admin, setAdmin] = useState(null);
+  const [teacher, setTeacher] = useState(null);
+  const [reviewer, setReviewer] = useState(null);
+  const [student, setStudent] = useState(null);
+  const location = useLocation();
+
+  useEffect(() => {
+    setAdmin(JSON.parse(localStorage.getItem('admin')));
+    setTeacher(JSON.parse(localStorage.getItem('teacher')));
+    setReviewer(JSON.parse(localStorage.getItem('reviewer')));
+    setStudent(JSON.parse(localStorage.getItem('student')));
+  }, [location]);
+  return (
+    <div>
+      <Routes>
+        {/* Admin Routes */}
+        <Route path='/admin' element={admin ? <AdminHome /> : <Navigate to='/admin/login' />}></Route>
+        <Route path='/admin/teachers' element={admin ? <TeacherList /> : <Navigate to='/admin/login' />}></Route>
+        <Route path='/admin/batches' element={admin?<Batches /> : <Navigate to='/admin/login'/>}></Route>
+        <Route path='/admin/login' element={admin ? <Navigate to='/admin' /> : <AdminLogin />}></Route>
+
+        {/* Teacher routes */}
+        {/* Teacher Auth */}
+        <Route path='/teacher/login' element={<TeacherLogin />}></Route>
+        <Route path='/teacher/register/:token' element={<TeacherSignup />} />
+        {/* Dashboard */}
+        <Route path='/teacher' element={<TeacherHome />} />
+        {/* Profile Management */}
+        <Route path='/teacher/profile/:id' element={<TeacherProfile />} />
+        <Route path='/teacher/profile/' element={<TeacherLogin />} />
+        <Route path='/teacher/editProfile/:id' element={<EditTeacherProfile />}></Route>
+        <Route path='/teacher/editProfile/:id' element={<EditTeacherProfile />} />
+        {/* Reviewer Management */}
+        <Route path='/teacher/reviewer' element={<Reviewer />} />
+
+        {/* Student management */}
+        <Route path='/teacher/students' element={<Students />}></Route>
+
+        {/* Domain management */}
+        <Route path='/teacher/domains' element={<Domains />}></Route>
+
+        {/* Student routes */}
+        <Route path='/' element={<StudentHome />}></Route>
+        <Route path='/signup' element={<StudentSignup />}></Route>
+        <Route path='/login' element={<StudentsLogin />}></Route>
+        <Route path='/profile' element={<StudentProfile />}></Route>
+        <Route path='/editProfile' element={<StudentEditProfile />}></Route>
+
+        {/* Reviewer Routes */}
+        <Route path='/reviewer' element={<ReviewerHome />} />
+        <Route path='/reviewer/login' element={<ReviewerLogin />} />
+        <Route path='/reviewer/register/:token' element={<ReviewerSignup />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default Routed;
