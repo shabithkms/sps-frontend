a6e022f547c768c91cf229e8ab743616d9db86b3
shabithkms <shabithkms2035@gmail.com>
2022-02-19 11:50:14 +0530

    Header styling for responsive

--- src/Components/Student/Header/Header.jsx
+++ src/Components/Student/Header/Header.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import { Navbar, Container, Nav } from "react-bootstrap";
-import { MenuItem, Menu } from "@mui/material";
-import "./Header.css";
-import { useNavigate } from "react-router";
+import React from 'react';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import { MenuItem, Menu } from '@mui/material';
+import './Header.css';
+import { useNavigate } from 'react-router';
 
 function Header() {
   let navigate = useNavigate();
@@ -28,31 +28,31 @@ function Header() {
   const handleMobileMenuOpen = (event) => {
     setMobileMoreAnchorEl(event.currentTarget);
   };
-  const doLogout=()=>{
-    localStorage.removeItem('student')
-    navigate('/login')
-  }
+  const doLogout = () => {
+    localStorage.removeItem('student');
+    navigate('/login');
+  };
 
-  const menuId = "primary-search-account-menu";
+  const menuId = 'primary-search-account-menu';
   const renderMenu = (
     <Menu
       anchorEl={anchorEl}
       anchorOrigin={{
-        vertical: "top",
-        horizontal: "right",
+        vertical: 'top',
+        horizontal: 'right',
       }}
       id={menuId}
       keepMounted
       transformOrigin={{
-        vertical: "top",
-        horizontal: "right",
+        vertical: 'top',
+        horizontal: 'right',
       }}
       open={isMenuOpen}
       onClose={handleMenuClose}
     >
       <MenuItem
         onClick={() => {
-          navigate("/profile");
+          navigate('/profile');
           handleMenuClose();
         }}
       >
@@ -63,34 +63,34 @@ function Header() {
   );
   return (
     <div>
-      <Navbar
-        collapseOnSelect
-        expand="lg"
-        bg="light"
-        className="text-dark shadow"
-      >
+      <Navbar collapseOnSelect expand='lg' bg='light' className='text-dark shadow'>
         <Container>
           <Navbar.Brand>
-            <span className="logo-header">SPS</span>
-            <span className="logo-dot">.</span>
+            <span className='logo-header'>SPS</span>
+            <span className='logo-dot'>.</span>
           </Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto"></Nav>
+          <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+          <Navbar.Collapse id='responsive-navbar-nav'>
+            <Nav className='me-auto'></Nav>
             <Nav>
               <Nav.Link>Tasks</Nav.Link>
               <Nav.Link>Manifest</Nav.Link>
               <Nav.Link>Timeline</Nav.Link>
-              <Nav.Link></Nav.Link>
               <Nav.Link>
-                <div onClick={handleProfileMenuOpen} className="pb-2">
-                  <img
-                    style={{ width: 50, height: 50 }}
-                    alt=""
-                    className="student-profile"
-                  />
-                </div>
+                <p className='  d-lg-none' onClick={() => navigate('/profile')}>
+                  {' '}
+                  Profile
+                </p>
+                <p className='  d-lg-none' onClick={() => doLogout()}>
+                  {' '}
+                  Logout
+                </p>
               </Nav.Link>
+              {/* <Nav.Link> */}
+                <div onClick={handleProfileMenuOpen} className='pb-2 d-none d-lg-block '>
+                  <img style={{ width: 50, height: 50 }} alt='' className='student-profile' />
+                </div>
+              {/* </Nav.Link> */}
             </Nav>
           </Navbar.Collapse>
         </Container>
