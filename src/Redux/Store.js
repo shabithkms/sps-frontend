4fdfba8a6c85505120626ee369437abc4982739c
shabithkms <shabithkms2035@gmail.com>
2022-02-22 21:29:26 +0530

    student reducer created

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import studentReducer from './studentSlice';
 
 export default configureStore({
-  reducer: {},
+  reducer: {
+    student: studentReducer,
+  },
 });
